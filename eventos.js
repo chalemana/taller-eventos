b84c853bf1d1c3b8a4bbc535ae7e138394e0eb71
Martina Schröder <chalemanaca@gmail.com>
2024-08-13 13:51:49 -0300

    agregar funciones js

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
-function saludar() {
-    alert('¡Hola!');
-}
+//En eventos.js agrega un manejador de eventos para el "click" en el div utilizando el método addEventListener. Debes programar que al dar click en el div salga una alerta con el texto "Hola! Soy el div".
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.button1').addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
+
